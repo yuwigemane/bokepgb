fa03a64b9ac387c261c13dfb68150942ebbec488
yuwigemane <yuwigemane@gmail.com>
2025-02-18 12:06:41 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-[bokepgb](https://bokepgb.pages.dev)
-Fork the [repository](https://github.com/lapelive) on GitHub.
+A live demo of the project is available at [https://bokepgb.pages.dev](https://bokepgb.pages.dev/).
+Fork the [repository](https://github.com/mutilbogeh) on GitHub.
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const DOODSTREAM_BASE_URL =
-    process.env.DOODSTREAM_BASE_URL || "https://doodapi.com";
-export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY || '460570fogp7ossqz4x520j';
+    process.env.DOODSTREAM_BASE_URL || "https://filemoonapi.com";
+export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY || '55526wwqd2gsfc4q0wgzl';
 export const SITENAME = process.env.SITENAME || "Bokep Gb";
 export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "20");
 export const DEFAULT_REVALIDATE_INTERVAL = 3600;
